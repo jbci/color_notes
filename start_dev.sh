7973c6d4c8fe81cd4f02c11ce0f984505a2cb6c0
Jordi Bari <jbari@ciudadanointeligente.org>
2016-04-11 22:07:41 +0000

    start script checks for running services

--- start_dev.sh
+++ start_dev.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #sudo service postgresql status
-sudo service postgresql start
-sudo service redis-server start
-cd notes
+output="$(sudo service postgresql status |grep online)" 
+if [ X"$output" = X"" ]; then
+  echo "launching database"
+  sudo service postgresql start
+fi
+output="$(sudo service redis-server status |grep not)" 
+if [ X"$output" != X"" ]; then
+  echo "launching redis server"
+  sudo service redis-server start
+fi
 rails s -b $IP -p $PORT
